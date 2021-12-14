d23430eb9e7fd65af6d904f532f274bb7e92ff23
Clemente Velasco <65301897+Arfentul@users.noreply.github.com>
2021-12-14 15:40:58 -0500

    Add files via upload
    Addition of NiftyMIC reconstruction scripts

---
+++ nm-gen.sh
@@ -0,0 +1,108 @@
+#!/bin/bash
+
+show_help () {
+cat << EOF
+    USAGE: sh ${0##*/} [niftymic case directory]
+    Incorrect input supplied
+    Required: In CASEFOLDER, create a directory
+    named 't2' and copy all input T2
+    stacks there, then run this script
+
+    Optional argument:
+        [ -a || --alpha ]   Set regularization for recon. Default is 0.01. 
+
+EOF
+}
+
+die() {
+        printf '%s\n' "$1" >&2
+        exit 1
+}
+
+while :; do
+    case $1 in
+        -h|-\?|--help)
+            show_help
+            exit
+            ;;
+        -a|--alpha)
+            if [[ "$2" ]] ; then
+                alpha=$2
+                echo Alpha set to $alpha
+                shift
+            else
+                die 'error: --alpha requires a number'
+                exit
+            fi
+            ;;
+         -?*)
+             printf 'warning: unknown option (ignored: %s\n' "$1" >&2
+             ;;
+         *) # Default case, no options
+             break
+     esac
+     shift
+done
+
+if [ $# -ne 1 ]; then
+    show_help
+    exit
+fi 
+
+# Set variables
+nmic=`readlink -f $1`
+t2dir="${nmic}/t2"
+if [ ! -d "${t2dir}" ]; then  
+    echo "$t2dir directory not present"
+    echo "Create this directory and copy T2 stacks there first"
+    exit
+fi
+maskdir="${nmic}/mask"
+mkdir -pv ${maskdir}
+srrdir="${nmic}/srr"
+if [[ ! -n $alpha ]] ; then
+    alpha="0.01"
+fi
+
+# Run scripts
+shsfb="${nmic}/run-sfb.sh"
+shpipe="${nmic}/run-nm.sh"
+if [ -f $shsfb ] ; then rm -v $shsfb ; fi
+if [ -f $shpipe ] ; then rm -v $shpipe ; fi
+
+# get list of stacks
+t2s=`find ${t2dir} -type f -name fetus_\*z`
+# write segment fetal brains script
+echo "#!/bin/bash" >> $shsfb
+echo "niftymic_segment_fetal_brains --filenames \\" >> $shsfb
+for stack in $t2s ; do
+    t2base=`basename $stack`
+	echo "t2/${t2base} \\" >> $shsfb
+done
+echo "--filenames-masks \\" >> $shsfb
+for stack in $t2s ; do
+    maskbase=`basename $stack` 
+	echo "mask/${maskbase} \\" >> $shsfb
+done
+
+# write reconstruction pipeline script including
+#   bias field correction
+#   subject space recon (niftymic_reconstruct_volume)
+#   template space alignment (niftymic_register_image)
+#   template space recon (niftymic_reconstruct_volume_from_slices)
+echo "#!/bin/bash" >> $shpipe
+echo "niftymic_run_reconstruction_pipeline --filenames \\" >> $shpipe
+for stack in $t2s ; do
+    t2base=`basename $stack`
+	echo "t2/${t2base} \\" >> $shpipe
+done
+echo "--filenames-masks \\" >> $shpipe
+for stack in $t2s ; do
+    maskbase=`basename $stack`
+	echo "mask/${maskbase} \\" >> $shpipe
+done
+echo "--dir-output srr/ \\" >> $shpipe
+echo "--alpha $alpha \\" >> $shpipe
+
+# Open permissions so docker sudo can access
+find ${nmic} -type d -exec chmod -c --preserve-root 777 {} \;
--- ucsf-findHASTE.sh
+++ ucsf-findHASTE.sh
@@ -32,14 +32,14 @@ for acq in ${nii}/* ; do
             continue
         fi
         echo "NIFTI: $im"
-        # Get dimensions (we only want 3D images
+        # Get dimensions (we only want 3D images)
         dims=`fslhd $im | grep dim0 | grep -v pix | sed 's,.*\([0-9]\),\1,g'`
         echo Number of dimensions: $dims
         if [[ ! $dims -eq 3 ]] ; then
             echo "Not 3D image - Skipping"
             continue
         fi
-        # Get image info
+        # Take dimension 1 (in plane) and 3 (out-of-plane) spacings
         dim1=`fslhd $im | grep pixdim1 | sed 's,.*dim1\?,,g'`
         dim3=`fslhd $im | grep pixdim3 | sed 's,.*dim3\?,,g'`
         echo "dim1 = $dim1 (we want 0.something here)"
@@ -47,12 +47,16 @@ for acq in ${nii}/* ; do
         ddim1=`echo $dim1 | sed 's,\..*,,g'`
         ddim3=`echo $dim3 | sed 's,\..*,,g'`
         echo "First digits: $ddim1 and $ddim3"
+        # T2's normally have have in plane thickness less than 1
+        # though I noticed some in this dataset are over 1(?), so I'm excluding 2 or greater
+        # For out of plane, I'm excluding those with thickness 4 or greater
         if [[ $ddim1 -gt 1 || $ddim3 -gt 3 ]] ; then
             echo "Image spacing isn't right for HASTE - Skipping"
             continue
         else echo "We shall copy $im"
             base=`basename $im`
             fetus="fetus${base}"
+            # Copy the selected images to the reconstruction directory
             cp $im -vn ${odir}/${fetus}
         fi
     fi

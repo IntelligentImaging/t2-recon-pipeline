69c0c5f4e83b122cb61b77ef67a74ff5909e2368
Clemente Velasco <65301897+Arfentul@users.noreply.github.com>
2024-01-16 12:20:15 -0500

    Add files via upload

--- svrtk-allexec.sh
+++ svrtk-allexec.sh
@@ -1,22 +1,48 @@
 #!/bin/bash
+
+die() {
+    printf '%s\n' "$1" >&2
+    exit 1
+}
+while :; do
+    case $1 in
+        -h|-\?|--help)
+            show_help # help message
+            exit
+            ;;
+        -s|--singularity)
+            let SING=1 
+            ;;
+        --) # end of optionals
+            shift
+            break
+            ;;
+        -)?*
+            printf 'warning: unknown option (ignored: %s\m' "$1" >&2
+            ;;
+        *) # default case, no optionals
+            break
+    esac
+    shift
+done
+
 if [ $# -ne 1 ]; then	
 	echo "Incorrect argument supplied!"
-	echo "usage: sh $0 [DATA MOUNT PATH]"
+	echo "usage: sh $0 [-s||--singularity] -- [DATA MOUNT PATH]"
     echo "This script is used after using svrtk-dock-gen.sh to generate multiple 'run-svrtk.sh'"
     echo "Supply a study folder which has cases organized like so:"
     echo "STUDY FOLDER"
     echo "    --CASE001/nii/run-svrtk.sh"
     echo "or  --CASE001/nii/group1/run-svrtk.sh"
-    echo "For each run-svrtk without a corresponding SVRTK.nii.gz recon,"
-    echo " creates a detached SVRTK docker image, and then uses it to execute the run script, then deletes the container"
+    echo "For each run-svrtk without a corresponding SVRTK.nii.gz recon, runs Docker/Singularity container SVRTK"
+    echo "-s || --singularity   Use Singularity instead of Docker. Must use this option for e2 server"  
 	exit
 fi
 
 begin=`pwd`
 DIR=$1
 if [[ ! -d $DIR ]] ; then
-    echo error: $DIR is not a directory
-    exit 1
+    die "error: $DIR is not a directory"
 fi
 
 echo "Searching for run-svrtk.sh scripts"
@@ -34,24 +60,28 @@ for f in $runs ; do
     if [ -f "${dname}/${output}" ] ; then
         echo $f already ran
     else
-        # # # RUN SVRTK IN DOCKER CONTAINER # # #
+        # # # RUN SVRTK IN CONTAINER # # #
         echo "Run $f"
         mpath=`readlink -f $dname` # Server directory to be mounted
-        conpath="/home/data" # Path to mount inside container
-        dockname="SVRTK-$RANDOM" # random string
-        echo "Container will be named $dockname"
-        echo "Mount path within container: $conpath"
-        echo "Initializing SVRTK Docker container"
-        docker run -id --name $dockname --rm --mount type=bind,source=${mpath},target=${conpath} fetalsvrtk/svrtk /bin/bash
-        echo
-        echo "Executing SVRTK run script within container"
-        date
-        docker exec -t -i -w /home/data $dockname sh -c "sh run-svrtk.sh"
-        echo
-        echo "Recon done"
-        date
-        echo "Stopping docker image"
-        docker stop $dockname
+
+        if [[ $SING = 1 ]] ; then
+            echo "Running singularity SVRTK"
+            singularity exec docker://fetalsvrtk/svrtk /bin/sh ${mpath}/run-svrtk.sh
+        else
+            conpath="/home/data" # Path to mount inside container
+            dockname="SVRTK-$RANDOM" # random string
+            echo "Container will be named $dockname"
+            echo "Mount path within container: $conpath"
+            echo "Initializing SVRTK Docker container"
+            docker run -id --name $dockname --rm --mount type=bind,source=${mpath},target=${conpath} fetalsvrtk/svrtk /bin/bash
+            echo
+            echo "Executing SVRTK run script within container"
+            docker exec -t -i -w /home/data $dockname sh -c "sh run-svrtk.sh"
+            echo
+            echo "Recon done"
+            echo "Stopping docker image"
+            docker stop $dockname
+        fi
         echo
     fi
 done

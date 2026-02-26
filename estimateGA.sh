ddcbb671dfc1733630342d85392fe177e3e8456f
Clemente Velasco <clemente.velasco@gmail.com>
2026-02-26 11:30:38 -0500

    hotfix missing var estimate GA

--- estimateGA.sh
+++ estimateGA.sh
@@ -11,6 +11,8 @@ input=`readlink -f $1`
 # A text file with the atlas volumes in column 2
 base=`basename $input`
 
+SHDIR=`dirname $0`
+
 # Binary threshold the recon to get a rough volume if no mask is supplied
 if [[ $2 == "-m" ]] ; then
     mask=$1
